58f0ffa6304a157551e066926562ed4e1ff808af
Adibayu Luthfiansyah <adibayuluthfiansyah@gmail.com>
2026-05-07 20:45:09 +0700

    fix auth login handler

--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -2,8 +2,13 @@ package service
 
 import (
 	"context"
+	"errors"
+	"os"
 
+	"cloud.google.com/go/auth/credentials/idtoken"
+	"github.com/morning-glorys/drav-backend/internal/model"
 	"github.com/morning-glorys/drav-backend/internal/repository"
+	"github.com/morning-glorys/drav-backend/pkg/utils"
 )
 
 type AuthService interface {
@@ -20,6 +25,42 @@ func NewAuthService(userRepo repository.UserRepository) AuthService {
 
 // google login
 func (s *authService) GoogleLogin(ctx context.Context, googleToken string) (string, error) {
-	return "", nil
-	// TODO: implementasi login dengan Google OAuth2
+	clientID := os.Getenv("GOOGLE_CLIENT_ID")
+	if clientID == "" {
+		return "", errors.New("Configuration Is Not Set")
+	}
+
+	payload, err := idtoken.Validate(ctx, googleToken, clientID)
+	if err != nil {
+		return "", errors.New("Invalid Google Token")
+	}
+
+	email, emailOk := payload.Claims["email"].(string)
+	name, nameOk := payload.Claims["name"].(string)
+
+	if !emailOk || !nameOk {
+		return "", errors.New("gagal mengekstrak email dan nama dari akun google")
+	}
+
+	user, err := s.userRepo.GetByUserEmail(ctx, email)
+	if err != nil {
+		newUser := &model.User{
+			Name:         name,
+			Email:        email,
+			AuthProvider: "google",
+			Role:         "user",
+		}
+		err = s.userRepo.CreateUser(ctx, newUser)
+		if err != nil {
+			return "", errors.New("gagal membuat akun baru")
+		}
+		user = newUser
+	}
+	token, err := utils.GenerateToken(user.ID, user.Role)
+	if err != nil {
+		return "", errors.New("gagal membuat sesi login")
+	}
+
+	return token, nil
+
 }
--- pkg/database/db.go
+++ pkg/database/db.go
@@ -3,6 +3,8 @@ package database
 import (
 	"database/sql"
 	"log"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func ConnectDB(dsn string) (*sql.DB, error) {
